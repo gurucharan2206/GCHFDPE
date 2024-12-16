9ce4fa6ebde2a7143236bc5ab6a6102dc80a7bef
gurucharan2206 <gurucharan2206@gmail.com>
2024-12-16 10:44:22 +0530

    interfaces for weatherstation

---
+++ KeepingObjectsInTheKnow/Interfaces.cs
@@ -0,0 +1,19 @@
+﻿namespace GCHFDPE.KeepingObjectsInTheKnow
+{
+    public interface IObserver
+    {
+        public void Update(float temp, float humidity, float pressure);
+    }
+
+    public interface ISubject
+    {
+        public void RegisterObserver(IObserver o);
+        public void RemoveObserver(IObserver o);
+        public void NotifyObservers();
+    }
+
+    public interface IDisplayElement
+    {
+        public void Display();
+    }
+}
