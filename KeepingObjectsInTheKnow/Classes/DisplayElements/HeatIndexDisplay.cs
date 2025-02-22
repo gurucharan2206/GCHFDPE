13d64bee7b968f9a014ec1d9c6176d26c477f84e
gurucharan2206 <gurucharan2206@gmail.com>
2025-02-23 00:35:57 +0530

    heat index last minute display element client requirement handled due to interface

---
+++ KeepingObjectsInTheKnow/Classes/DisplayElements/HeatIndexDisplay.cs
@@ -0,0 +1,27 @@
+﻿using GCHFDPE.KeepingObjectsInTheKnow.Interfaces;
+
+namespace GCHFDPE.KeepingObjectsInTheKnow.Classes.DisplayElements
+{
+    public class HeatIndexDisplay : IDisplayElement, IObserver
+    {
+        public float _apparentTemperature { get; set; }
+        public WeatherData _weatherData { get; set; }
+
+        public HeatIndexDisplay(WeatherData weatherData)
+        {
+            this._weatherData = weatherData;
+            _weatherData.RegisterObserver(this);
+        }
+
+        public void Update(float temp, float humidity, float pressure)
+        {
+            this._apparentTemperature = temp / (temp + humidity);
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine(@$"Heat Index: {this._apparentTemperature} ");
+        }
+    }
+}
