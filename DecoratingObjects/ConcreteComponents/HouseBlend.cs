6cebc5992b5d32c47582a3d59055729da372210e
gurucharan2206 <gurucharan2206@gmail.com>
2025-02-23 23:40:31 +0530

    Component, Decorator, ConcreteComponent, ConcreteDecorator

---
+++ DecoratingObjects/ConcreteComponents/HouseBlend.cs
@@ -0,0 +1,17 @@
+﻿using GCHFDPE.DecoratingObjects.AbstractClasses;
+
+namespace GCHFDPE.DecoratingObjects.Classes
+{
+    public class HouseBlend : Beverage
+    {
+        public HouseBlend()
+        {
+            _description = "House Blend";
+        }
+
+        public override double Cost()
+        {
+            return 2.60;
+        }
+    }
+}
