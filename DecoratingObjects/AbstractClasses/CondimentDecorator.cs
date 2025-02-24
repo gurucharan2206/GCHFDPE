e9fbc95154698c3b77a958c0026b906459aadc80
gurucharan2206 <gurucharan2206@gmail.com>
2025-02-24 21:35:24 +0530

    revert ch3

--- DecoratingObjects/AbstractClasses/CondimentDecorator.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GCHFDPE.DecoratingObjects.AbstractClasses
-{
-    public abstract class CondimentDecorator : Beverage
-    {
-        protected Beverage _beverage;
-
-        public abstract override string GetDescription();
-    }
-}
